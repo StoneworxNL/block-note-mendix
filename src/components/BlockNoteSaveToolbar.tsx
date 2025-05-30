79c401a4688f94e18c17ebc57dcfcba9b8aae500
João Lopes <joao.lopes@nedpt.com>
2025-05-30 12:31:34 +0100

    Updated state management according to documentation

--- src/components/BlockNoteSaveToolbar.tsx
+++ src/components/BlockNoteSaveToolbar.tsx
@@ -11,6 +11,77 @@ export interface BlockNoteSaveProps {
 export function BlockNoteSaveToolbar({ jsonAttribute, saveAction, editor }: BlockNoteSaveProps): ReactElement {
     
     const handleSave = () => {
+        if (saveAction && !saveAction.isExecuting) {
+            if (saveAction.canExecute) {
+                jsonAttribute.setValue(
+                    JSON.stringify(editor.document, null, 2)
+                );
+                saveAction.execute();
+            } else {
+                console.log('Save not possible as Save action cannot be executed.');
+            }
+        } else {
+            console.log('Save not possible as Save action is either unavailable or executing.');
+        }
+    };
+
+    return (
+        <div className="blocknote-save-btn">
+            <button className="btn mx-button" onClick={handleSave}>Save</button>
+        </div>
+    );
+
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import { ReactElement, createElement } from "react";
+import { BlockNoteEditor } from "@blocknote/core";
+import { ActionValue, EditableValue } from "mendix";
+
+export interface BlockNoteSaveProps {
+    jsonAttribute: EditableValue<string>;
+    saveAction?: ActionValue;
+    editor: BlockNoteEditor;
+}
+
+export function BlockNoteSaveToolbar({ jsonAttribute, saveAction, editor }: BlockNoteSaveProps): ReactElement {
+    
+    const handleSave = () => {
         if(saveAction && !saveAction.isExecuting)   {
             if(saveAction.canExecute)   {
                 jsonAttribute.setValue(
@@ -34,6 +105,8 @@ export function BlockNoteSaveToolbar({ jsonAttribute, saveAction, editor }: Bloc
     );
 
 }
+
+*/
 
 
 
--- src/components/BlockNoteWrapper.tsx
+++ src/components/BlockNoteWrapper.tsx
@@ -19,6 +19,104 @@ export function BlockNoteWrapper({ jsonAttribute, saveAction, isEditable, themeE
     const [initialContent, setInitialContent] = useState<PartialBlock[] | undefined | "loading">("loading");
 
     // Loads stored editor contents (https://www.blocknotejs.org/examples/backend/saving-loading)
+    
+    useEffect(() => {
+        if (jsonAttribute && jsonAttribute.status === "available") {
+            setInitialContent((jsonAttribute.value
+                ? JSON.parse(jsonAttribute.value.toString())
+                : JSON.parse(defaultContent)) as PartialBlock[]);
+        }
+    }, [jsonAttribute]);
+
+    // Creates a new editor instance.
+    // We use useMemo + createBlockNoteEditor instead of useCreateBlockNote so we
+    // can delay the creation of the editor until the initial content is loaded.
+    
+    const editor = useMemo(() => {
+        if (initialContent === "loading")
+            return undefined;
+    
+        return BlockNoteEditor.create({ initialContent });
+    }, [initialContent]);
+    
+    if (editor === undefined) {
+        return <div>Loading content...</div>;
+    }
+
+    // Renders the editor instance.
+
+    return (
+        <div className={'blocknote-mendix-wrapper blocknote-mx-' + themeEnum}>
+            {isEditable && (
+                <BlockNoteSaveToolbar 
+                    jsonAttribute={jsonAttribute}
+                    saveAction={saveAction}
+                    editor={editor}
+                />
+            )}
+            <BlockNoteView
+                editor={editor}
+                editable={isEditable}
+                theme={themeEnum === 'light' ? 'light' : 'dark'}
+            />
+        </div>
+    );
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import { ReactElement, createElement, useEffect, useState, useMemo } from "react";
+import { BlockNoteView } from "@blocknote/mantine";
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { BlockNoteSaveToolbar } from "./BlockNoteSaveToolbar";
+import { ActionValue, EditableValue } from "mendix";
+import "@blocknote/core/fonts/inter.css";
+import "@blocknote/mantine/style.css";
+
+export interface BlockNoteProps {
+    jsonAttribute: EditableValue<string>;
+    saveAction?: ActionValue;
+    isEditable: boolean;
+    themeEnum: string;
+}
+
+export function BlockNoteWrapper({ jsonAttribute, saveAction, isEditable, themeEnum }: BlockNoteProps): ReactElement {
+    
+    const defaultContent = '[{"type": "paragraph","props": {"textColor": "default","backgroundColor": "default","textAlignment": "left"},"content": [],"children": []}]';
+    const [initialContent, setInitialContent] = useState<PartialBlock[] | undefined | "loading">("loading");
+
+    // Loads stored editor contents (https://www.blocknotejs.org/examples/backend/saving-loading)
     useEffect(() => {
         if (jsonAttribute.status === "available") {
             setInitialContent((jsonAttribute.value
@@ -59,3 +157,4 @@ export function BlockNoteWrapper({ jsonAttribute, saveAction, isEditable, themeE
         </div>
     );
 }
+*/
