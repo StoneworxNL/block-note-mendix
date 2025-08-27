07124e0f0f556a07b4a56e4b4d06fb4fc37f34a3
João Lopes <joao.lopes@nedpt.com>
2025-08-27 21:25:14 +0100

    Adding new property types to import/export data from mendix domain model objects

--- typings/BlockNoteProps.d.ts
+++ typings/BlockNoteProps.d.ts
@@ -4,7 +4,7 @@
  * @author Mendix Widgets Framework Team
  */
 import { CSSProperties } from "react";
-import { ActionValue, EditableValue } from "mendix";
+import { ActionValue, EditableValue, ListValue, ListAttributeValue, ListReferenceValue } from "mendix";
 
 export type ThemeEnumEnum = "light" | "dark";
 
@@ -13,6 +13,16 @@ export interface BlockNoteContainerProps {
     class: string;
     style?: CSSProperties;
     tabIndex?: number;
+    blocksDataSource: ListValue;
+    blockId: ListAttributeValue<string>;
+    blockType: ListAttributeValue<string>;
+    contentItemDataSource: ListValue;
+    contentItemAssociation: ListReferenceValue;
+    contentItemType: ListAttributeValue<string>;
+    contentItemText: ListAttributeValue<string>;
+    stylesDataSource: ListValue;
+    stylesAssociation: ListReferenceValue;
+    isBold: ListAttributeValue<boolean>;
     jsonAttribute: EditableValue<string>;
     isEditable: boolean;
     saveAction?: ActionValue;
@@ -30,6 +40,16 @@ export interface BlockNotePreviewProps {
     readOnly: boolean;
     renderMode: "design" | "xray" | "structure";
     translate: (text: string) => string;
+    blocksDataSource: {} | { caption: string } | { type: string } | null;
+    blockId: string;
+    blockType: string;
+    contentItemDataSource: {} | { caption: string } | { type: string } | null;
+    contentItemAssociation: string;
+    contentItemType: string;
+    contentItemText: string;
+    stylesDataSource: {} | { caption: string } | { type: string } | null;
+    stylesAssociation: string;
+    isBold: string;
     jsonAttribute: string;
     isEditable: boolean;
     saveAction: {} | null;
