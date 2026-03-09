7d906ba54856bf37a795fdfcd7574ce2f6914f1e
João Lopes <joao.lopes@nedpt.com>
2026-03-09 11:36:16 +0000

    after npm install

--- src/BlockNote.tsx
+++ src/BlockNote.tsx
@@ -1,21 +1,14 @@
 import { ReactElement, createElement } from "react";
-import { BlockNoteWrapper } from "./components/BlockNoteWrapper";
 import { BlockNoteContainerProps } from "../typings/BlockNoteProps";
-
+import { BlockNoteWrapper } from "./components/BlockNoteWrapper";
 import "./ui/BlockNote.css";
 
-export function BlockNote({ jsonPayload, saveAction, isEditable, themeEnum, blocksDataSource, blockId, blockType, contentItemAssociation, contentItemDataSource }: BlockNoteContainerProps): ReactElement {
+export function BlockNote({ jsonPayload, saveAction, isEditable, themeEnum }: BlockNoteContainerProps): ReactElement {
     return <BlockNoteWrapper 
                 jsonPayload={jsonPayload}
                 saveAction={saveAction}
                 isEditable={isEditable}
                 themeEnum={themeEnum}
-                // Blocks data source props
-                blocksDataSource={blocksDataSource}
-                blockId={blockId}
-                blockType={blockType}
-                // Content item data source props
-                contentItemDataSource={contentItemDataSource}
-                contentItemAssociation={contentItemAssociation}
+
         />;
 }
--- src/components/BlockNoteWrapper.tsx
+++ src/components/BlockNoteWrapper.tsx
@@ -2,25 +2,25 @@ import { ReactElement, createElement, useEffect, useState, useMemo } from "react
 import { BlockNoteView } from "@blocknote/mantine";
 import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
 import { BlockNoteSaveToolbar } from "./BlockNoteSaveToolbar";
+import { ActionValue, EditableValue } from "mendix";
 import "@blocknote/core/fonts/inter.css";
 import "@blocknote/mantine/style.css";
-import { ActionValue, EditableValue, ListValue, ListAttributeValue, ListReferenceValue } from "mendix";
 
 export interface BlockNoteProps {
     jsonPayload: EditableValue<string>;
     saveAction?: ActionValue;
     isEditable: boolean;
     themeEnum: string;
-    // Blocks data source props
+    /*// Blocks data source props
     blocksDataSource: ListValue;
     blockId: ListAttributeValue<string>;
     blockType: ListAttributeValue<string>;
     // Content item data source props
     contentItemDataSource: ListValue;
-    contentItemAssociation: ListReferenceValue;
+    contentItemAssociation: ListReferenceValue;*/
 }
 
-export function BlockNoteWrapper({ jsonPayload, saveAction, isEditable, themeEnum, blocksDataSource }: BlockNoteProps): ReactElement {
+export function BlockNoteWrapper({ jsonPayload, saveAction, isEditable, themeEnum }: BlockNoteProps): ReactElement {
     
     const defaultContent = '[{"type": "paragraph","props": {"textColor": "default","backgroundColor": "default","textAlignment": "left"},"content": [],"children": []}]';
     const [initialContent, setInitialContent] = useState<PartialBlock[] | undefined | "loading">("loading");
@@ -33,7 +33,7 @@ export function BlockNoteWrapper({ jsonPayload, saveAction, isEditable, themeEnu
                 ? JSON.parse(jsonPayload.value.toString())
                 : JSON.parse(defaultContent)) as PartialBlock[]);
         }
-    }, [blocksDataSource]);
+    }, [jsonPayload]);
 
     // Creates a new editor instance.
     // We use useMemo + createBlockNoteEditor instead of useCreateBlockNote so we
